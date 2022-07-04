785f23898c39c652e56d0d4095b3730ac8c17e62
32817 <328170849@qq.com>
2022-07-04 22:35:40 +0800

    https://gitee.com/ordinary-malisha/book_shop.git

--- src/Servlet/AdminServlet.java
+++ src/Servlet/AdminServlet.java
@@ -17,6 +17,7 @@ public class AdminServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+
         this.doPost(request,response);
     }
 
